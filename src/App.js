50ee9f2a92b9b1fec5104fc29558336333330e8b
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2022-12-01 21:00:48 +0330

    dummy expenses added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const DUMMY_EXPENSES = [
   { id: 1, title: "Book", amount: 294.67, date: new Date(2022, 2, 28) },
   { id: 2, title: "Video Game ", amount: 300.67, date: new Date(2021, 2, 11) },
   { id: 3, title: "car BMW", amount: 555.67, date: new Date(2020, 2, 20) },
+  { id: 4, title: "Library", amount: 2500, date: new Date(2020, 2, 2) },
 ];
 function App() {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
